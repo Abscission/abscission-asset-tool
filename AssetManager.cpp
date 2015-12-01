617f5845a9b366d5a43f3e6ae2a2ecb104464c6b
Jason Light <jason@abscission.net>
2015-12-01 21:34:50 +1300

    It updated

--- AssetManager.cpp
+++ AssetManager.cpp
@@ -1,7 +1,14 @@
 
 #include <Windows.h>
 #include <iostream>
+#include <vector>
+#include <string>
 #include "include\zlib.h"
+
+struct File {
+	std::string Filename;
+	bool shouldCompress;
+};
 
 #pragma pack(push, 1)
 struct Header {
@@ -9,22 +16,48 @@ struct Header {
 
 	int NumberOfEntries;
 	int DataLength;
-
-	bool Compressed;
 };
 
 struct IndexEntry {
 	char Name[12];
 	int Position;
 	int Length;
+	bool Compressed;
 };
 #pragma pack(pop)
 
 typedef unsigned char Byte;
 
 int main(int argc, char* argv[]) {
-	int NumberOfAssets = argc - 2;
-	
+	std::vector<File> Assets;
+
+	//parse arguments
+	bool compressNext = true;
+	for (int i = 1; i < argc - 1; i++) {
+		bool compress = compressNext;
+
+		if (strcmp(argv[i], "-c") == 0) {
+			compressNext = true;
+		}
+		else if (strcmp(argv[i], "-nc") == 0) {
+			compressNext = false;
+		}
+		else {
+			File a = { argv[i], compress };
+			Assets.push_back(a);
+			compressNext = true;
+		}
+	}
+
+	const std::string usingCompression = " using compression";
+	const std::string noCompression = " without compression";
+
+	for (auto asset : Assets) {
+		std::cout << "Adding " << asset.Filename << (asset.shouldCompress ? usingCompression : noCompression) << '\n';
+	}
+
+	int NumberOfAssets = Assets.size();
+
 	if (NumberOfAssets < 1) {
 		std::cout << "Usage: " << argv[0] << " [asset1] [asset2]... [assetN] [AssetFile.aaf]";
 		return 1;
@@ -33,7 +66,7 @@ int main(int argc, char* argv[]) {
 	Header FileHeader = {};
 	*(int*)FileHeader.FileID = 0x41454741;
 	FileHeader.NumberOfEntries = NumberOfAssets;
-	FileHeader.Compressed = true;
+	//FileHeader.Compressed = true;
 
 	IndexEntry* Indexes = (IndexEntry*)VirtualAlloc(0, sizeof(IndexEntry) * NumberOfAssets, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	Byte** CompressedFiles = (Byte**)VirtualAlloc(0, sizeof(char*) * NumberOfAssets, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
@@ -47,7 +80,8 @@ int main(int argc, char* argv[]) {
 	//For each asset
 	for (int i = 0; i < NumberOfAssets; i++){
 		//Open the file
-		HANDLE AssetFile = CreateFileA(argv[i + 1], GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+		HANDLE AssetFile = CreateFileA(Assets[i].Filename.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
 		if (AssetFile == INVALID_HANDLE_VALUE) {
 			std::cout << "Failed to open file: " << argv[i + 1] << '\n';
 			return 1;
@@ -61,13 +95,24 @@ int main(int argc, char* argv[]) {
 		//Allocate the compression boundary amount of memory. This is the maximum size the file could expand to if it is incompressable data.
 		int MaxSize = compressBound(FileSize);
 		CompressedFiles[i] = (Byte*)VirtualAlloc(0, MaxSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+		
+		unsigned long CompressedSize;
 
-		unsigned long CompressedSize = MaxSize;
-		compress(CompressedFiles[i], &CompressedSize, (Byte*)UncompressedData, FileSize);
-		std::cout << "\nFile:  " << argv[i + 1] << "\nOriginalSize: " << FileSize << " bytes\nCompressedSize: " << CompressedSize << " bytes\n\n";
+		if (Assets[i].shouldCompress) {
+			CompressedSize = MaxSize;
+			compress(CompressedFiles[i], &CompressedSize, (Byte*)UncompressedData, FileSize);
+			std::cout << "\nFile:  " << Assets[i].Filename << "\nOriginalSize: " << FileSize << " bytes\nCompressedSize: " << CompressedSize << " bytes\n\n";
+		}
+		else {
+			CompressedSize = FileSize;
+			memcpy(CompressedFiles[i], UncompressedData, FileSize);
+			std::cout << "\nFile:  " << Assets[i].Filename << "\nSize: " << FileSize << " bytes\nNot Compressed\n\n";
+
+		}
 
 		Indexes[i].Length = CompressedSize + 8;
 		Indexes[i].Position = RunningPosition;
+		Indexes[i].Compressed = Assets[i].shouldCompress;
 
 		RunningPosition += CompressedSize + 8;
 
@@ -86,8 +131,10 @@ int main(int argc, char* argv[]) {
 	memcpy((void*)((char*)FileBuffer + sizeof(FileHeader)), Indexes, sizeof(IndexEntry) * NumberOfAssets);
 	
 	for (int i = 0; i < NumberOfAssets; i++) {
-		*(int*)((char*)FileBuffer + sizeof(Header) + (sizeof(IndexEntry) * NumberOfAssets) + Indexes[i].Position) = OriginalSizes[i];
-		*((int*)((char*)FileBuffer + sizeof(Header) + (sizeof(IndexEntry) * NumberOfAssets) + Indexes[i].Position) + 1) = CompressedSizes[i];
+		if (Assets[i].shouldCompress) {
+			*(int*)((char*)FileBuffer + sizeof(Header) + (sizeof(IndexEntry) * NumberOfAssets) + Indexes[i].Position) = OriginalSizes[i];
+			*((int*)((char*)FileBuffer + sizeof(Header) + (sizeof(IndexEntry) * NumberOfAssets) + Indexes[i].Position) + 1) = CompressedSizes[i];
+		}
 		memcpy((void*)((int*)((char*)FileBuffer + sizeof(Header) + (sizeof(IndexEntry) * NumberOfAssets) + Indexes[i].Position) + 2), CompressedFiles[i], CompressedSizes[i]);
 	}
 
